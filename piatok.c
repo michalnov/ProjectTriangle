868cb7ccb8f81fdb4f0967d80033c59236392707
michal.novotny <michal.novotny@akademiasovy.sk>
2017-10-21 11:36:51 +0200

    V2

--- piatok.c
+++ piatok.c
@@ -16,6 +16,7 @@ int main()
 	int arr[D], a=0, i=0, even=0, i2digit=0, i3digit=0, pa=0, pal[D], dev7=0, min=0, max=0;
 	for(a = 0; a < D; a++){
 		arr[a]=rand()%9990+10;
+		printf("%d ", arr[a]);
 		if(arr[a] % 7 == 0)
 		{
 			dev7++;			
@@ -66,13 +67,13 @@ int main()
 			}
 		}
 	}
-	printf(" %d numbers are even.\n", even);
+	printf("\n %d numbers are even.\n", even);
 	printf(" %d numbers are odd.\n", D - even);
 	printf(" there is %d 2 digits, %d 3 digits and %d 4 digits numbers.\n", i2digit, i3digit, (D - (i2digit + i3digit)));
 	printf(" %d is maximum.\n", max);
 	printf(" %d is min.\n", min);
 	printf("\nReverse of array:\n");
-	for(a = D-1; a > 0; a--)
+	for(a = D-1; a >= 0; a--)
 	{
 		printf("%d ", arr[a]);
 	}
